d8e54edd13618b2d1e25bca13043c5d17681ffe9
hungdinh <hungdinh@qasymphony.com>
2018-03-04 18:13:57 +0700

    ''

--- public/components/util.js
+++ public/components/util.js
@@ -15,22 +15,22 @@ module.exports = {
   parseAddress: (addressComponents) => {
     let address = {};
     addressComponents.forEach(component => {
-      if (component.types[0] == 'country') {
+      if (component.types[0] == 'country' && component.long_name) {
         address.country = component.long_name;
       }
-      if (component.types[0] == 'administrative_area_level_1') {
+      if (component.types[0] == 'administrative_area_level_1' && component.long_name) {
         address.city = component.long_name;
       }
-      if (component.types[0] == 'administrative_area_level_2') {
+      if (component.types[0] == 'administrative_area_level_2' && component.long_name) {
         address.district = component.long_name;
       }
-      if (component.types[0] == 'administrative_area_level_3') {
+      if (component.types[0] == 'administrative_area_level_3' && component.long_name) {
         address.ward = component.long_name;
       }
-      if (component.types[0] == 'street_number') {
+      if (component.types[0] == 'street_number' && component.long_name) {
         address.street = component.long_name;
       }
-      if (component.types[0] == 'route') {
+      if (component.types[0] == 'route' && component.long_name) {
         address.street += ' ' + component.long_name;
       }
     });
